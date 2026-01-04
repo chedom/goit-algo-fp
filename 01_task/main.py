5d291344857e4a861a10cf7417afb15bb3d19335
Vasyl Domanchuk <domanchukits@gmail.com>
2026-01-04 23:27:28 +0100

    implement reverse operation

--- 01_task/main.py
+++ 01_task/main.py
@@ -1,5 +1,21 @@
+from linked_list import LinkedList
+
+
 def main():
-    pass
+    reverse_linked_list()
+
+
+def reverse_linked_list():
+    llist = LinkedList()
+
+    # Вставляємо вузли в початок
+    llist.insert_at_beginning(5)
+    llist.insert_at_beginning(10)
+    llist.insert_at_beginning(15)
+
+    llist.print_list()
+    llist.reverse()
+    llist.print_list()
 
 
 if __name__ == "__main__":
