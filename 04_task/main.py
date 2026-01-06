bbdc228b1a46b6fbb19bf9599981e1662a2da210
Vasyl Domanchuk <domanchukits@gmail.com>
2026-01-07 00:29:19 +0100

    implement task 4

--- 04_task/main.py
+++ 04_task/main.py
@@ -1,5 +1,80 @@
+import uuid
+import heapq
+
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+class Node:
+    def __init__(self, key, color="skyblue"):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.color = color  # Додатковий аргумент для зберігання кольору вузла
+        self.id = str(uuid.uuid4())  # Унікальний ідентифікатор для кожного вузла
+
+
+def add_edges(graph, node, pos, x=0, y=0, layer=1):
+    if node is not None:
+        graph.add_node(node.id, color=node.color, label=node.val)  # Використання id та збереження значення вузла
+        if node.left:
+            graph.add_edge(node.id, node.left.id)
+            l = x - 1 / 2 ** layer
+            pos[node.left.id] = (l, y - 1)
+            l = add_edges(graph, node.left, pos, x=l, y=y - 1, layer=layer + 1)
+        if node.right:
+            graph.add_edge(node.id, node.right.id)
+            r = x + 1 / 2 ** layer
+            pos[node.right.id] = (r, y - 1)
+            r = add_edges(graph, node.right, pos, x=r, y=y - 1, layer=layer + 1)
+    return graph
+
+
+def draw_tree(tree_root):
+    tree = nx.DiGraph()
+    pos = {tree_root.id: (0, 0)}
+    tree = add_edges(tree, tree_root, pos)
+
+    colors = [node[1]['color'] for node in tree.nodes(data=True)]
+    labels = {node[0]: node[1]['label'] for node in tree.nodes(data=True)}  # Використовуйте значення вузла для міток
+
+    plt.figure(figsize=(8, 5))
+    nx.draw(tree, pos=pos, labels=labels, arrows=False, node_size=2500, node_color=colors)
+    plt.show()
+
+
+def convert_heap_to_tree(heap):
+    if not heap:  # empty heap
+        return
+    # preinitialize nodes without left/right children
+    nodes = [Node(val) for val in heap]
+    size = len(nodes)
+
+    idx = 0
+    last_parent_idx = (size - 2) / 2
+
+    while idx <= last_parent_idx:
+        # 2i + 1 - left element
+        # 2i + 2 - right element
+        left_idx = 2*idx + 1
+        rigth_idx = 2*idx + 2
+
+        nodes[idx].left = nodes[left_idx]
+        # rigth child could be absent
+        if rigth_idx <= size - 1:
+            nodes[idx].right = nodes[rigth_idx]
+
+        idx += 1
+
+    return nodes[0]  # return head
+
+
 def main():
-    pass
+    heap = [0, 4, 5, 10, 1, 3]
+    heapq.heapify(heap)
+    print(heap)
+    root = convert_heap_to_tree(heap)
+    draw_tree(root)
 
 
 if __name__ == "__main__":
